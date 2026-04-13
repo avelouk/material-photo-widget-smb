22e3de1ee8343260b8f2e58509ebdbac8decf03c
Filipe Belatti <fibelatti@gmail.com>
2026-04-13 18:24:44 +0100

    Update `runWithFileOutputStream`
    - Add `mkdirs` fallback, in an attempt to fix crashes
    - Make it thread safe, despite all callers already being thread safe

--- app/src/main/java/com/fibelatti/photowidget/platform/FileKtx.kt
+++ app/src/main/java/com/fibelatti/photowidget/platform/FileKtx.kt
@@ -2,18 +2,25 @@ package com.fibelatti.photowidget.platform
 
 import java.io.File
 import java.io.FileOutputStream
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 
-fun File.runWithFileOutputStream(operation: (FileOutputStream) -> Unit) {
-    if (!exists()) createNewFile()
+suspend inline fun File.runWithFileOutputStream(crossinline operation: (FileOutputStream) -> Unit) {
+    withContext(Dispatchers.IO) {
+        if (!exists()) {
+            mkdirs()
+            createNewFile()
+        }
 
-    runCatching {
-        FileOutputStream(this).use(operation)
-        require(this.length() > 0) { "File is empty" }
-    }.onSuccess {
-        Timber.d("Successfully operated on $this")
-    }.onFailure {
-        Timber.e(it, "runWithFileOutputStream failed on $path.")
-        delete()
+        runCatching {
+            FileOutputStream(this@runWithFileOutputStream).use(operation)
+            require(length() > 0) { "File is empty" }
+        }.onSuccess {
+            Timber.d("Successfully operated on $this")
+        }.onFailure {
+            Timber.e(it, "runWithFileOutputStream failed on $path.")
+            delete()
+        }
     }
 }
